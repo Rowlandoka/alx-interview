04f0f97c1c312f7e7d9ead0d7e0189745ead2ee7
Rowland Oka <rowlandoka@gmail.com>
2023-10-01 07:07:15 +0100

    setup project directory and initialise

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
 """Pascal"""
 
+
 def pascal_triangle(n):
-  """Function to get the number of pascal triangle"""
+    """fucnrion to get the number of pascal triangle"""
+    if n <= 0:
+        return []
 
-  if n <= 0:
-    return []
-  
-  pascal_triangle = [0] * n
-  
-  for i in range(n):
-    row = [0] * (i+1)
-    row[0] = 1
-    row[len(row) - 1] = 1
+    pascal_triangle = [0] * n
 
-    for j in range(1, i):
-      if j > 0 and j < len(row):
-        a = pascal_triangle[i - 1][j]
-        b = pascal_triangle[i - 1][j - 1]
-        row[j] = a + b
+    for i in range(n):
+        new_row = [0] * (i+1)
+        new_row[0] = 1
+        new_row[len(new_row) - 1] = 1
 
-    pascal_triangle[i] = row
+        for j in range(1, i):
+            if j > 0 and j < len(new_row):
+                a = pascal_triangle[i - 1][j]
+                b = pascal_triangle[i - 1][j - 1]
+                new_row[j] = a + b
 
-  return pascal_triangle
+        pascal_triangle[i] = new_row
+
+    return pascal_triangle
